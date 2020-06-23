2d40f0d64c2e261c29024acd46752b40f9271839
Stathis Xantinidis <spithash@hotmail.com>
2020-06-23 16:14:35 +0300

    Update TMG.sh

--- TMG.sh
+++ TMG.sh
@@ -24,12 +24,12 @@ echo -e "Adding \e[1mProper Tabs\e[0m"
 sed -i -e 's/^/\t\t\t\t\t\t/' ip-blacklist.xml
 echo -e "\e[2mAdded Proper Tabs\e[0m"
 echo -e "\e[93mCleaning Trash Files\e[0m"
-#rm inserted_prefix.txt
-#rm ipsum.txt
-#rm nocomments.txt
-#rm added_needed_tabs.txt
-#rm removed_digit_at_end.txt
-#rm removed_tabs_and_spaces.txt
+rm inserted_prefix.txt
+rm ipsum.txt
+rm nocomments.txt
+rm added_needed_tabs.txt
+rm removed_digit_at_end.txt
+rm removed_tabs_and_spaces.txt
 echo -e "\e[93mTrash Files Cleaned\e[0m"
 echo -e "\e[93mListing TempFile:\e[0m"
 du -sh ip-blacklist.xml
