30ba6e01bc4de2760d4d2be8d4252aaf9077b935
Stathis Xantinidis <spithash@hotmail.com>
2020-06-29 13:27:17 +0300

    Added some statements and error outputs. The script now stops if something goes wrong. 	modified:   TMG.sh

--- TMG.sh
+++ TMG.sh
@@ -1,42 +1,82 @@
 #!/bin/bash
 # TMG IP Blacklist XML Generator
 
-cd ~/TMG-Blacklist-Generator && echo -e "\e[1mFetching Blacklist\e[0m..."
+cd ~/TMG-Blacklist-Generator 
+echo -e "\n"
+echo -e "\e[1mFetching Blacklist\e[0m...\n"
 wget https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt
 
-echo -e "Removing \e[1mComments\e[0m"
-sed -e '/^#/d' ipsum.txt > nocomments.txt && echo -e "\e[2mComments Removed\e[0m"
+echo -e "\e[1mRemoving Comments\e[0m"
+	if sed -e '/^#/d' ipsum.txt > nocomments.txt ; then
+		echo -e "\e[2mComments Removed\e[0m"
+	else
+		echo -e "Error: \e[91mCould not remove comments\e[0m" && exit 0
+	fi
 
-echo -e "Removing \e[1mLines That End With 10\e[0m"
-sed -i '/10$/d' nocomments.txt && echo -e "\e[2mLines Containing 10s At The End Removed\e[0m"
+echo -e "\e[1mRemoving Lines That End With 10\e[0m"
+	if sed -i '/10$/d' nocomments.txt ; then
+		echo -e "\e[2mLines Containing 10s At The End Removed\e[0m"
+	else
+		echo -e "Error: \e[91mCould not remove 10's from file\e[0m" && exit 0
+	fi
 
-echo -e "Removing \e[1mUnnecessary Numbers\e[0m"
-sed 's/.$//' nocomments.txt > removed_digit_at_end.txt && echo -e "\e[2mCleaned Unnecessary Lines\e[0m"
+echo -e "\e[1mRemoving Unnecessary Numbers\e[0m"
+	if sed 's/.$//' nocomments.txt > removed_digit_at_end.txt ; then
+		echo -e "\e[2mCleaned Unnecessary Lines\e[0m"
+	else
+		echo -e "Error: \e[91mCould not remove digit at end\e[0m" && exit 0
+	fi
 
-echo -e "Removing \e[1mSpaces & Tabs\e[0m"
-sed 's/[[:blank:]]*$//' removed_digit_at_end.txt > removed_tabs_and_spaces.txt && echo -e "\e[2mCleaned Spaces & Tabs\e[0m"
+echo -e "\e[1mRemoving Spaces & Tabs\e[0m"
+	if sed 's/[[:blank:]]*$//' removed_digit_at_end.txt > removed_tabs_and_spaces.txt ; then
+		echo -e "\e[2mCleaned Spaces & Tabs\e[0m"
+	else
+		echo -e "Error: \e[91mCould not clean spaces & tabs\e[0m" && exit 0
+	fi
 
-echo -e "Inserting \e[1mPrefix\e[0m"
-sed -e 's/^/<fpc4:Str dt:dt="string">http:\/\//' removed_tabs_and_spaces.txt > inserted_prefix.txt && echo -e "\e[2mDone Inserting Prefix\e[0m"
+echo -e "\e[1mInserting Prefix\e[0m"
+	if sed -e 's/^/<fpc4:Str dt:dt="string">http:\/\//' removed_tabs_and_spaces.txt > inserted_prefix.txt ; then
+		echo -e "\e[2mDone Inserting Prefix\e[0m"
+	else
+		echo -e "Error: \e[91mCould not insert prefix\e[0m" && exit 0
+	fi
+ 
+echo -e "\e[1mInserting Suffix\e[0m"
+	if awk '{print $0"</fpc4:Str>"}' inserted_prefix.txt > ip-blacklist.xml ; then
+		echo -e "\e[2mDone Inserting Suffix\e[0m"
+	else
+		echo -e "Error: \e[91mCould not insert suffix\e[0m" && exit 0
+	fi
 
-echo -e "Inserting \e[1mSuffix\e[0m"
-awk '{print $0"</fpc4:Str>"}' inserted_prefix.txt > ip-blacklist.xml && echo -e "\e[35mDone Inserting Suffix\e[0m"
+echo -e "\e[1mAdding Proper Tabs\e[0m"
+	if sed -i -e 's/^/\t\t\t\t\t\t/' ip-blacklist.xml ; then 
+		echo -e "\e[2mAdded Proper Tabs\e[0m"
+	else
+		echo -e "Error: \e[91mCould not add tabs\e[0m" && exit 0
+	fi
 
-echo -e "Adding \e[1mProper Tabs\e[0m"
-sed -i -e 's/^/\t\t\t\t\t\t/' ip-blacklist.xml && echo -e "\e[2mAdded Proper Tabs\e[0m"
+echo -e "\e[1mCleaning Trash Files\e[0m"
+	if rm inserted_prefix.txt && rm ipsum.txt && rm nocomments.txt && rm removed_digit_at_end.txt && rm removed_tabs_and_spaces.txt ; then
+		echo -e "\e[2mTrash Files Cleaned\e[0m"
+	else
+		echo -e "Error: \e[91mCould not clean trash files\e[0m" && exit 0
+	fi
 
-echo -e "\e[93mCleaning Trash Files\e[0m"
-rm inserted_prefix.txt && rm ipsum.txt && rm nocomments.txt && rm removed_digit_at_end.txt && rm removed_tabs_and_spaces.txt && echo -e "\e[93mTrash Files Cleaned\e[0m"
+echo -e "\e[1mListing TempFile:\e[0m"
+du -sh ip-blacklist.xml
 
-echo -e "\e[93mListing TempFile:\e[0m"
-du -sh ip-blacklist.xml && echo -e "\e[91mCombining XML Files\e[0m"
+	if sed '/<fpc4:URLStrings>/ r ip-blacklist.xml' sample_tmg.xml > final.xml ; then
+		echo -e "\e[93mCombining XML files\e[0m"
+	else
+		echo -e "Error: \e[91mCould not combine files\e[0m" && exit 0
+	fi
 
-sed '/<fpc4:URLStrings>/ r ip-blacklist.xml' sample_tmg.xml > final.xml && sleep 7
+sleep 7
 
-echo -e "\e[93mCleaning Temp XML File\e[0m"
+echo -e "\e[2mCleaning Temp XML File\e[0m"
 rm ip-blacklist.xml
 
-echo -e "\e[93mListing Final File:\e[0m"
+echo -e "\e[2mListing Final File:\e[0m"
 du -sh final.xml
 
 echo -e "\e[32mDone!\e[0m"
